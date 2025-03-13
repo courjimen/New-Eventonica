6a1166763866333ec78895d99261bda0b2abcb26
courjimen <coureyfrance@gmail.com>
2025-03-13 19:06:04 -0400

    post command working on postman

--- server/server.js
+++ server/server.js
@@ -5,18 +5,32 @@ const cors = require('cors');
 const port = 3000;
 
 app.use(cors());
+app.use(express.json());
 //grabs data for events
 
 app.get('/events', async (req, res) => {
     try {
         const result = await pool.query('SELECT * FROM events');
         res.json(result.rows);
+
     } catch (err) {
         console.error('You have an error: ', err);
     }
 })
 
-const location = 
+//allows user to edit events
+app.post('/events', async (req, res) => {
+    const {location, date, time} = req.body;
+  
+    try{
+      const createEvent = await pool.query('INSERT INTO events (location, date, time) VALUES ($1, $2, $3) RETURNING *', [location, date, time]);
+      res.json(createEvent.rows);
+
+    } catch(err) {
+      console.error(err);
+      res.status("Unable to create your event due").send(err);
+    }
+  });
 
 
 app.listen(port, () => {
