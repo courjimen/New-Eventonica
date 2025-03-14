19ea1816c543f9df8e997d4e4dc8dc87af84acd7
courjimen <coureyfrance@gmail.com>
2025-03-14 17:31:44 -0400

    added css to data pulled

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
+    
      <Search />
       <Delete />
       <Form />
--- client/src/components/Butters/Butters.jsx
+++ client/src/components/Butters/Butters.jsx
@@ -15,18 +15,23 @@ function Butters() {
   }
 
   return (
-    <div>
-    <h2>Whipped Butters</h2>
+    <div className="flex flex-col items-center">
+    <h2 className="text-2xl font-bold mb-4">Whipped Butters</h2>
+    <div className="flex flex-wrap justify-center">
       <ul data-testid="list">
+        <div>
         {butters.map(butter => (
-          <li key={butter.id}>
-            Scents: {butter.scents}, 
-            Color: {butter.color},
-            Quantity: {butter.quantity}
+          <li key={butter.id}
+          className="rounded-full p-4 border border-orange-300 flex flex-col items-center justify-center text-center m-2">
+            <div className="mb-1">Scents: {butter.scents}</div>
+            <div className="mb-1">Color: {butter.color}</div>
+            <div>Quantity: {butter.quantity}</div>
           </li>
         ))}
+        </div>
       </ul>
-      <button data-testid="button" onClick={handleClick}>See Whipped Butters</button>
+      </div>
+      <button className="mt-4 bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" data-testid="button" onClick={handleClick}>See Whipped Butters</button>
         {err && <p>{err}</p>}
     </div>
   )
