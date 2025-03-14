a1e5a18fa96ea7f75f6957eeb596b3d860c39d84
courjimen <coureyfrance@gmail.com>
2025-03-14 00:15:02 -0400

    added UI to remove butter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,54 @@
 import Butters from './components/Butters';
 import Form from './components/Form'
+import Search from './components/Search'
+import { useState } from 'react'
 import './App.css'
 
 
 function App() {
-  
+const [deleteButter, setDeleteButter] = useState('');
 
-return (
-  <>
-    <Form />
-    <Butters />
+const removeButter = () => {
+  fetch('http://localhost:3000/butters', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ scents: deleteButter }),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      alert('Butter removed!');
+    })
+    .catch((error) => {
+      console.error('Error', error)
+      alert('Unable to remove butter')
+    })
+  }
 
-  </>
+  return (
+    <>
+      <label>Find a Scent: </label>
+      <input
+        placeholder='Insert Scent'
 
-)
+      />
+<div>
+      <label> Remove a Scent: </label>
+      <input
+        placeholder='Insert scent to delete'
+        type='text'
+        value={deleteButter}
+        onChange={(e) => setDeleteButter(e.target.value)}
+      />
+      <button onClick={removeButter}>Delete Butter</button>
+      </div>
+      <Form />
+      <Butters />
+
+    </>
+
+  )
 }
 
 export default App
